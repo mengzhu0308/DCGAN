bc6ad61f528c1cf10ea4e0fdd47852c4ec55b6a3
Meng <75422140+mengzhu0308@users.noreply.github.com>
2021-01-21 20:26:10 +0800

    Update generator_model.py

--- codes/generator_model.py
+++ codes/generator_model.py
@@ -13,15 +13,16 @@ def generator_model(x, out_imgsize=(28, 28, 1)):
     out_h, out_w, out_c = out_imgsize
     h, w, = out_h // 4, out_w // 4
 
-    x = Dense(h * w * 224, kernel_initializer=RandomNormal())(x)
+    x = Dense(h * w * 224, use_bias=False, kernel_initializer=RandomNormal())(x)
     x = Reshape((h, w, 224))(x)
-    x = LeakyReLU(alpha=0.2)(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
 
     x = UpSampling2D()(x)
 
     x = Conv2D(112, 5, padding='same', use_bias=False, kernel_initializer=RandomNormal())(x)
     x = BatchNormalization()(x)
-    x = LeakyReLU(alpha=0.2)(x)
+    x = ReLU()(x)
 
     x = UpSampling2D()(x)
 
